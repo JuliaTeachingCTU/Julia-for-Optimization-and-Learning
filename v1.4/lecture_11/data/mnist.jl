2658f0e11056894bed70be75b7874320a6bed4eb
Documenter.jl <documenter@juliadocs.github.io>
2025-09-22 10:53:12 +0000

    build based on 8827cfb

---
+++ v1.4/lecture_11/data/mnist.jl
@@ -0,0 +1,54 @@
+using Flux
+using Flux: onecold
+using MLDatasets
+
+include(joinpath(dirname(@__FILE__), ("utilities.jl")))
+
+T = Float32
+dataset = MLDatasets.MNIST
+
+X_train, y_train, X_test, y_test = load_data(dataset; T=T, onehot=true)
+
+model = Chain(
+    Conv((2, 2), 1 => 16, sigmoid),
+    MaxPool((2, 2)),
+    Conv((2, 2), 16 => 8, sigmoid),
+    MaxPool((2, 2)),
+    Flux.flatten,
+    Dense(288, size(y_train, 1)),
+    softmax,
+)
+
+file_name = evaldir("mnist_sigmoid.jld2")
+train_or_load!(file_name, model, X_train, y_train)
+
+ii1 = findall(onecold(y_train, 0:9) .== 1)[1:5]
+ii2 = findall(onecold(y_train, 0:9) .== 9)[1:5]
+
+for qwe = 0:9
+    ii0 = findall(onecold(y_train, 0:9) .== qwe)[1:5]
+
+    p0 = [plot_image(X_train[:, :, :, i:i][:, :, 1, 1]) for i in ii0]
+    p1 = [plot_image((model[1:2](X_train[:, :, :, i:i]))[:, :, 1, 1]) for i in ii0]
+    p2 = [plot_image((model[1:4](X_train[:, :, :, i:i]))[:, :, 1, 1]) for i in ii0]
+
+    p = plot(p0..., p1..., p2...; layout=(3, 5))
+    display(p)
+end
+
+p0 = [plot_image(X_train[:, :, :, i:i][:, :, 1, 1]) for i in ii1]
+p1 = [plot_image((model[1:2](X_train[:, :, :, i:i]))[:, :, 1, 1]) for i in ii1]
+p2 = [plot_image((model[1:4](X_train[:, :, :, i:i]))[:, :, 1, 1]) for i in ii1]
+
+plot(p0..., p1..., p2...; layout=(3, 5))
+
+
+p0 = [plot_image(X_train[:, :, :, i:i][:, :, 1, 1]) for i in ii2]
+p1 = [plot_image((model[1:2](X_train[:, :, :, i:i]))[:, :, 1, 1]) for i in ii2]
+p2 = [plot_image((model[1:4](X_train[:, :, :, i:i]))[:, :, 1, 1]) for i in ii2]
+
+plot(p0..., p1..., p2...; layout=(3, 5))
+
+for i in 1:length(model)
+    println(size(model[1:i](X_train[:, :, :, 1:1])))
+end
--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
+  "v1.4",
   "v1.3",
   "v1.2",
   "v1.1",
@@ -8,5 +9,5 @@ var DOC_VERSIONS = [
   "v0.8",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v1.3";
+var DOCUMENTER_NEWEST = "v1.4";
 var DOCUMENTER_STABLE = "stable";
